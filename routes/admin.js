d931be378258acfc17e3d221ff2188d36e4744b7
phat <phatvo@dayoneteams.com>
2019-11-10 01:47:23 +0700

    feat: admin page with account and courses manager

--- routes/admin.js
+++ routes/admin.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+  res.render('admin/account', { layout: 'admin' });
+});
+
+router.get('/course', function (req, res, next) {
+  res.render('admin/course', { layout: 'admin' });
 });
 
 module.exports = router;
