30b751245e03acbc8cf71279fef782a5e26cca95
mnhnhttrn <minhnhattranw@gmail.com>
2019-11-19 19:17:09 +0700

    update course proc

--- database_scripts/store_procs/reads.sql
+++ database_scripts/store_procs/reads.sql
@@ -8,3 +8,10 @@ BEGIN
 END;$$
 DELIMITER ;
 
+DELIMITER $$
+USE `clever`$$
+CREATE PROCEDURE GetCourseDetail(in courseId int)
+BEGIN
+	select * from courses where course_id = courseId and courses_status = 'active';
+END;$$
+DELIMITER ;
