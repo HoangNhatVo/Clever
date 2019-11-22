bc0e4d5ea0c5965601e88d68343c752594a07995
mnhnhttrn <minhnhattranw@gmail.com>
2019-11-22 21:33:25 +0700

    update procs

--- database_scripts/store_procs/reads.sql
+++ database_scripts/store_procs/reads.sql
@@ -23,3 +23,13 @@ BEGIN
     select * from subjects where subject_status = 'active' ;
 END;$$
 DELIMITER ;
+
+DELIMITER $$
+USE `clever`$$
+CREATE PROCEDURE GetActiveCourseListBySubject(in pageNth int, in subjectID int)
+BEGIN
+	declare l int default 12;
+	if (pageNth < 1) then set pageNth = 1; end if;
+    select * from courses join accounts join co_ac join subjects where courses_status = 'active' and course_subject = subject_id and course_id = co_ac_course and co_ac_account = account_id and co_ac_role = 'teacher' and subject_id = subjectID;
+END;GetActiveCourseListBySubject$$
+DELIMITER ;
