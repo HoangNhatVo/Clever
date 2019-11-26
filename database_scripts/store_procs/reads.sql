957af51f2a41b70fc03625da033bce3bcbb95309
mnhnhttrn <minhnhattranw@gmail.com>
2019-11-26 22:12:02 +0700

    update proc get details of account

--- database_scripts/store_procs/reads.sql
+++ database_scripts/store_procs/reads.sql
@@ -41,3 +41,12 @@ BEGIN
     select * from subjects where subject_status = 'active' and subject_id = subjectID;
 END;$$
 DELIMITER ;
+
+DELIMITER $$
+USE `clever`$$
+CREATE DEFINER=`dev`@`localhost` PROCEDURE `GetAccountDetails`(in accountID int(11))
+BEGIN
+	select account_id,account_status,account_role,account_email,account_phone,account_username,account_first_name,account_last_name,account_address,account_birthdate, sum(transaction_amount) as 'balance' from accounts left join transactions on account_id = transaction_account group by account_id;
+END$$
+
+DELIMITER ;
